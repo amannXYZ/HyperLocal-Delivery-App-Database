b4fa18eed8c1a9acab56e871c0883dfe45d0af8b
Aman Kumar <amannkr09@gmail.com>
2026-08-05 23:04:00 +0530

    Update Notification_Handelling.sql

--- Notification_Handelling.sql
+++ Notification_Handelling.sql
@@ -9,13 +9,13 @@ CREATE TABLE Notifications(
 
 INSERT INTO Notifications (Message, ScheduledTime, DayOfWeek)
 VALUES
-('Your burger misses you 🍔', '09:00:00', 'Monday'),
-('Pizza is calling... answer now 🍕', '13:00:00', 'Tuesday'),
-('Diet starts tomorrow 😅', '20:00:00', 'Wednesday'),
-('Biryani detected nearby 👀', '19:30:00', 'Thursday'),
-('Late night cravings unlocked 🌙', '23:00:00', 'Friday'),
-('Weekend + Food = Perfect ❤️', '12:00:00', 'Saturday'),
-('Sunday special? Order now 😋', '14:00:00', 'Sunday');
+('Your burger misses you ', '09:00:00', 'Monday'),
+('Pizza is calling... answer now', '13:00:00', 'Tuesday'),
+('Diet starts tomorrow', '20:00:00', 'Wednesday'),
+('Biryani detected nearby', '19:30:00', 'Thursday'),
+('Late night cravings unlocked', '23:00:00', 'Friday'),
+('Weekend + Food = Perfect', '12:00:00', 'Saturday'),
+('Sunday special? Order now', '14:00:00', 'Sunday');
 
 SELECT * FROM Notifications;
 
